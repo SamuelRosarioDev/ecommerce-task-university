131b7bd608023e6b51b15863c713cf00db1eec40
samuelrosariodev <samuelrosario.dev@gmail.com>
2024-11-03 15:39:25 -0300

    bug cors

--- ecommerce-backend/src/server.js
+++ ecommerce-backend/src/server.js
@@ -1,27 +1,30 @@
 import fs from "node:fs";
 import path from "node:path";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import jwt from "jsonwebtoken";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 
-require("dotenv").config();
+dotenv.config(); // Carregar variáveis de ambiente
 
 const port = 3000;
 const app = express();
 
-const allowedOrigin = 'https://ecommerce-task-university.vercel.app';
+const allowedOrigin = "https://ecommerce-task-university.vercel.app";
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (origin === allowedOrigin || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(
+	cors({
+		origin: (origin, callback) => {
+			if (origin === allowedOrigin || !origin) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+	}),
+);
 app.use(express.json());
 
 const databaseUser = path.join(__dirname, "./database/Users.json");
@@ -43,24 +46,31 @@ app.post("/cadastro", (request, response) => {
 	const { nome, email, senha } = request.body;
 
 	fs.readFile(databaseUser, "utf8", (err, data) => {
-		let users = [];
+		if (err) {
+			return response
+				.status(500)
+				.json({ message: "Erro ao ler o arquivo de usuários" });
+		}
 
+		let users = [];
 		if (data) {
 			users = JSON.parse(data);
 
 			const emailExists = users.filter((user) => user.email === email);
 			if (emailExists.length > 0) {
-				// email ja existe nao crie outra conta com esse email
-				return response.status(404).json({ message: "usuario ja existe" });
+				return response.status(404).json({ message: "Usuário já existe" });
 			}
 		}
 
 		const newUser = { id: uuidv4(), nome, email, senha, admin: false };
-		console.log(senha);
-		
 		users.push(newUser);
 
 		fs.writeFile(databaseUser, JSON.stringify(users, null, 4), (err) => {
+			if (err) {
+				return response
+					.status(500)
+					.json({ message: "Erro ao cadastrar o usuário" });
+			}
 			response
 				.status(201)
 				.json({ message: "Usuário cadastrado com sucesso!", user: newUser });
@@ -72,14 +82,25 @@ app.post("/login", (request, response) => {
 	const { email, senha } = request.body;
 
 	fs.readFile(databaseUser, "utf8", (err, data) => {
+		if (err) {
+			return response
+				.status(500)
+				.json({ message: "Erro ao ler o arquivo de usuários" });
+		}
+
 		if (data) {
 			const users = JSON.parse(data);
-
 			const verifyLogin = users.filter(
 				(user) => user.email === email && user.senha === senha,
 			);
 
 			if (verifyLogin.length > 0) {
+				if (!process.env.JWT_SECRET) {
+					return response
+						.status(500)
+						.json({ message: "Chave secreta não configurada" });
+				}
+
 				const token = jwt.sign(
 					{ id: verifyLogin[0].id, email: verifyLogin[0].email },
 					process.env.JWT_SECRET,
@@ -103,11 +124,17 @@ app.post("/addProdutos", upload.single("imgFile"), (request, response) => {
 	const { nomeProduto, valor, quantidade } = request.body;
 	const imgFile = request.file;
 
+	if (!imgFile) {
+		return response.status(400).send("Nenhuma imagem enviada");
+	}
+
 	fs.readFile(databaseProducts, "utf8", (err, data) => {
-		
-		if (!imgFile) {
-			return response.status(400).send("Nenhuma imagem enviada");
+		if (err) {
+			return response
+				.status(500)
+				.json({ message: "Erro ao ler o arquivo de produtos" });
 		}
+
 		let products = [];
 		if (data) {
 			products = JSON.parse(data);
@@ -118,6 +145,7 @@ app.post("/addProdutos", upload.single("imgFile"), (request, response) => {
 				return response.status(404).json({ message: "Produto já existe" });
 			}
 		}
+
 		const newProducts = {
 			nomeProduto,
 			valor,
@@ -125,10 +153,13 @@ app.post("/addProdutos", upload.single("imgFile"), (request, response) => {
 			imgFile: imgFile.path,
 		};
 		products.push(newProducts);
-		
+
 		fs.writeFile(databaseProducts, JSON.stringify(products, null, 4), (err) => {
-			console.log("teste");
-			
+			if (err) {
+				return response
+					.status(500)
+					.json({ message: "Erro ao adicionar o produto" });
+			}
 			response.status(201).json({ ...newProducts });
 		});
 	});
@@ -136,10 +167,18 @@ app.post("/addProdutos", upload.single("imgFile"), (request, response) => {
 
 app.get("/produtos", (request, response) => {
 	fs.readFile(databaseProducts, "utf8", (err, data) => {
+		if (err) {
+			return response
+				.status(500)
+				.json({ message: "Erro ao ler o arquivo de produtos" });
+		}
+
 		if (data) {
 			const produtos = JSON.parse(data);
 			return response.status(200).json({ produtos });
 		}
+
+		return response.status(404).json({ message: "Nenhum produto encontrado" });
 	});
 });
 
