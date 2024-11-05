104d1b225857e9bbe7dce1683e840d935fdd608c
samuelrosariodev <samuelrosario.dev@gmail.com>
2024-11-05 14:36:02 -0300

    bug

--- ecommerce-backend/src/server.js
+++ ecommerce-backend/src/server.js
@@ -11,10 +11,10 @@ dotenv.config();
 
 const port = 3000;
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', './index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.use(cors({ 
