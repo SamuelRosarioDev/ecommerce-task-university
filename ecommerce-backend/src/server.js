7589f57f78ec1fffd39372461e6db2182984bb70
samuelrosariodev <samuelrosario.dev@gmail.com>
2024-11-05 14:13:20 -0300

    bug

--- ecommerce-backend/src/server.js
+++ ecommerce-backend/src/server.js
@@ -11,6 +11,11 @@ dotenv.config();
 
 const port = 3000;
 const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(cors({ 
 	origin: "https://ecommerce-task-university.vercel.app", 
